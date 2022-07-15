a3b663c1e239ccd280f30f5b46562f2cf8ce198c
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-15 22:44:03 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        public class SubscriptionStatus
+        {
+            public bool Status { get; set; }
+
+            public DateTime ExpireDate { get; set; }
+        }
+
+        public static void Main()
+        {
+            var x = new SubscriptionStatus();
+            x = null;
+            var result = x?.Status == true ? true : false;
+            Console.WriteLine(result);
+            
+        }
+    }
+}
