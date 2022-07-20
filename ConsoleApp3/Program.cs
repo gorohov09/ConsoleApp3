6e6838dbf2cafb71aa09eea9c4c5c5b3dbd6ad66
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-07-20 14:26:49 +0500

    fix

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,25 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace ConsoleApp3
 {
     public class Program
     {
-        public class SubscriptionStatus
+        public static void Main(string[] args)
         {
-            public bool Status { get; set; }
+            string[] commands = new string[]
+            {
+                "push Привет! Это снова я! Пока!",
+                "pop 5",
+                "push Как твои успехи? Плохо?",
+                "push qwertyuiop",
+                "push 1234567890",
+                "pop 26"
+            };
 
-            public DateTime ExpireDate { get; set; }
+            Console.WriteLine(ApplyCommands(commands));
         }
 
-        public static void Main()
+        private static string ApplyCommands(string[] commands)
         {
-            var x = new SubscriptionStatus();
-            x = null;
-            var result = x?.Status == true ? true : false;
-            Console.WriteLine(result);
-            
+            var resultStr = new StringBuilder();
+            foreach (var command in commands)
+            {
+                var words = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (words.Length > 0)
+                {
+                    if (words[0] == "push")
+                    {
+                        for (int i = 0; i < words.Length; i++)
+                        {
+                            if (i == 0)
+                                continue;
+                            if (i == words.Length - 1)
+                            {
+                                resultStr.Append(words[i]);
+                                continue;
+                            }
+                                
+                            resultStr.Append($"{words[i]} ");
+                        }
+                    }
+                    else if (words[0] == "pop")
+                    {
+                        var delete_count = Convert.ToInt32(words[1]);
+                        if (resultStr.Length > delete_count)
+                        {
+                            resultStr.Remove(resultStr.Length - delete_count, delete_count);
+                        }
+                    }
+                }
+            }
+            return resultStr.ToString();
         }
+
     }
 }
